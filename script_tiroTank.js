2df09e0bea9345b43e5bde244a96d290cae4317d
Jorge Wagner <jorgetrabalho2021@gmail.com>
2023-12-30 21:19:17 -0300

    Adicionado tempo limite para o tiro do tank

--- script_tiroTank.js
+++ script_tiroTank.js
@@ -1,22 +1,92 @@
-// document.addEventListener("keydown", function tiroDoTank1(event){
-//     if(event.key == " "){
-//         criacaoDoTiro()
-//         tiroTank.style.display = "block"
-//         // var interval = window.setInterval(animate, 100);    
-//         let possitionTiroTank1 = parseInt(getComputedStyle(tiroTank).left)
-//     }
-// })
+function criacaoDoTiro() {
+  var tiroTank = document.createElement("span")
+  tiroTank.id = "tiroTank"
+  document.body.appendChild(tiroTank)
+  tiroTank.style.display = "block"  
+}
 
-// // function animate() {
-// //     increment = -25
-// //     let possitionTiroTank1 = parseInt(getComputedStyle(tiroTank1).left)
-// //     tiroTank1.style.left = possitionTiroTank1 + increment + "px";   
-// // }
-  
-// function criacaoDoTiro(){
-//     var tiroTank = document.createElement("span")
-//     tiroTank.id = "tiroTank"
-//     document.body.appendChild(tiroTank)
-//     let tiroTankPositionHorizontal = parseInt(getComputedStyle(tank1).left);
-//     let tiroTankPositionVertical = parseInt(getComputedStyle(tank1).top);
-// }
+function position_animacaoDoTiro() {
+  let tiroTankPositionHorizontal = parseInt(getComputedStyle(tank1).left);
+  let tiroTankPositionVertical = parseInt(getComputedStyle(tank1).top);
+
+  if (tank1.style.transform == "rotate(90deg)") { // Movimento para cima
+
+    tiroTank.style.left = tiroTankPositionHorizontal + 40 + "px"
+    tiroTank.style.top = tiroTankPositionVertical - 20 + "px"
+    tiroTank.style.transform = 'rotate(90deg)';
+    var movimentoTiro = setInterval(animacaoTiroTankMovimentoParaCima, 100);
+
+    setTimeout(() => {
+      clearTimeout(movimentoTiro);
+      document.body.removeChild(tiroTank)
+    }, 5000);
+
+  } else if (tank1.style.transform == "rotate(180deg)") { // Movimento para direita
+
+    tiroTank.style.left = tiroTankPositionHorizontal + 90 + "px"
+    tiroTank.style.top = tiroTankPositionVertical + 20 + "px"
+    tiroTank.style.transform = 'rotate(180deg)';
+    var movimentoTiro = setInterval(animacaoTiroTankMovimentoParaDireita, 100);
+
+      setTimeout(() => {
+        clearTimeout(movimentoTiro);
+        document.body.removeChild(tiroTank)
+      }, 5000);    
+
+  } else if (tank1.style.transform == "rotate(-90deg)") { // Movimento para baixo
+
+    tiroTank.style.left = tiroTankPositionHorizontal + 40 + "px"
+    tiroTank.style.top = tiroTankPositionVertical + 60 + "px"
+    tiroTank.style.transform = 'rotate(-90deg)';
+    var movimentoTiro = setInterval(animacaoTiroTankMovimentoParaBaixo, 100);
+
+    setTimeout(() => {
+      clearTimeout(movimentoTiro);
+      document.body.removeChild(tiroTank)
+    }, 5000);
+
+
+  } else if (tank1.style.transform == "rotate(0deg)" || tank1.style.transform == "") { // Movimento para esquerda
+    tiroTank.style.left = tiroTankPositionHorizontal + "px"
+    tiroTank.style.top = tiroTankPositionVertical + 20 + "px"
+    tiroTank.style.transform = 'rotate(0deg)';
+    var movimentoTiro = setInterval(animacaoTiroTankMovimentoParaEsquerda, 100);
+
+    setTimeout(() => {
+      clearTimeout(movimentoTiro);
+      document.body.removeChild(tiroTank)
+    }, 5000);
+
+  }
+}
+
+function animacaoTiroTankMovimentoParaCima() {
+
+  increment = -25
+  let possitionTiroTank1 = parseInt(getComputedStyle(tiroTank).top)
+  tiroTank.style.top = possitionTiroTank1 + increment + "px";
+
+}
+
+function animacaoTiroTankMovimentoParaDireita() {
+
+  increment = +25
+  let possitionTiroTank1 = parseInt(getComputedStyle(tiroTank).left)
+  tiroTank.style.left = possitionTiroTank1 + increment + "px";
+}
+
+function animacaoTiroTankMovimentoParaBaixo() {
+
+  increment = +25
+  let possitionTiroTank1 = parseInt(getComputedStyle(tiroTank).top)
+  tiroTank.style.top = possitionTiroTank1 + increment + "px";
+}
+
+function animacaoTiroTankMovimentoParaEsquerda() {
+
+  increment = -25
+  let possitionTiroTank1 = parseInt(getComputedStyle(tiroTank).left)
+  tiroTank.style.left = possitionTiroTank1 + increment + "px";
+
+}
+
